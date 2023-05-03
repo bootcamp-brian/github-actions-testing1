30e8a67c373e2e928f4c5223e82d1948d6a3d27f
brian <braintm2@gmail.com>
2023-05-03 14:57:31 -0700

    testing in local website repo

--- github-actions/copy-file-to-new-repo.js
+++ github-actions/copy-file-to-new-repo.js
@@ -3,33 +3,36 @@ const { Octokit } = require("@octokit/rest");
 const token = process.env.token;
 const octokit = new Octokit({ auth: token });
 
-// Copies contents of code of conduct source file and copies them over to a target repo
-// Needs arguments repo: 'name of target repo', path: 'path of file to copy contents into'
+// Copies contents of code of conduct source file and creates/updates it in designated repos
 async function updateCodeOfConduct(repos) {
     const owner = 'bootcamp-brian';
-    const codeOfConductSource = await octokit.request(`GET /repos/${owner}/github-actions-testing-2/contents/README.md`);
+    // Gets contents of code of conduct source file
+    const codeOfConductSource = await octokit.request(`GET /repos/${owner}/codeofconduct/contents/README.md`);
 
     const { content } = codeOfConductSource.data;
 
+    // loops through array of provided repos
     for (let repo of repos) {
         const { name, path } = repo;
         let currentCodeOfConduct = false;
 
+        // Checks to see if a file already exists at the designated path
         try {
             currentCodeOfConduct = await octokit.request(`GET /repos/${owner}/${name}/contents/${path}`);
         } catch (error) {
             console.log(`${error.response.url} not found`)
         }
 
+        // Uses contents of the source file to update existing file or create a new file
         if (currentCodeOfConduct) {
             const { sha } = currentCodeOfConduct.data;
-            const response = await octokit.request(`PUT /repos/${owner}/${name}/contents/${path}`, {
+            await octokit.request(`PUT /repos/${owner}/${name}/contents/${path}`, {
                 sha,
                 message: 'Updating code of conduct',
                 content
             });
         } else {
-            const response = await octokit.request(`PUT /repos/${owner}/${name}/contents/${path}`, {
+            await octokit.request(`PUT /repos/${owner}/${name}/contents/${path}`, {
                 message: 'Updating code of conduct',
                 content
             });
@@ -37,16 +40,20 @@ async function updateCodeOfConduct(repos) {
     }
 }
 
+// Defines repos and corresponding paths to copy the code of conduct into
+// To add additional repos, add an object to this array with the following structure:
+// {
+//     name: '[name of repo]',
+//     path: '[path to the file you want to create/update]'
+// }
+    // Note:
+    // The token being accessed from hackforla/codeofconduct's secrets.GHA_TOKEN
+    // needs to have write access to the repositories in this repos array
 const repos = [
     {
-        name: 'github-actions-testing1',
-        path: '_includes/code-of-conduct-page/CODEOFCONDUCT.md'
-    },
-    {
-        name: 'github-actions-testing-2',
+        name: 'website',
         path: '_includes/code-of-conduct-page/CODEOFCONDUCT.md'
     },
 ]
 
-// Run updateCodeofConduct() for all repos that need a copy of the code of conduct
 updateCodeOfConduct(repos);
